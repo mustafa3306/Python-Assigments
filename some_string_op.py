b1ebce693ee78095b7cc7d34d561575057689576
mustafa3306 <mustafaworkkok@gmail.com>
2022-03-27 16:50:12 +0300

    another function was added

--- some_string_op.py
+++ some_string_op.py
@@ -6,3 +6,9 @@ def front_back(word):
     else:
         return word[len(word)-1] + word[1:(len(word)-1)] + word[0]
 front_back("a")
+
+"""this function removes the specified letter"""
+def missing_char(word, n):
+    name = list(word)
+    name.pop(n)
+    return "".join(name)
