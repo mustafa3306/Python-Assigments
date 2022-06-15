3f85aec28f33765032bf274c3fed1f9a4b4be437
mustafa3306 <mustafaworkkok@gmail.com>
2022-06-15 10:28:22 +0300

    edits

--- some_string_op.py
+++ some_string_op.py
@@ -1,13 +1,19 @@
 """ this function aims to replace the first letter of the given word with the 
-last letter""" 
+last letter"""
+
+
 def front_back(word):
-    if len(word)<= 1:
+    if len(word) <= 1:
         return word
     else:
         return word[len(word)-1] + word[1:(len(word)-1)] + word[0]
+
+
 front_back("a")
 
 """this function removes the specified letter"""
+
+
 def missing_char(word, n):
     name = list(word)
     name.pop(n)
